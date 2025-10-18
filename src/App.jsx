9765ca0d3599dc3b7b72e366482d15e5e87915b5
Akbar-Widya <akbarwidya@student.telkomuniversity.ac.id>
2025-10-18 20:25:42 +0700

    Implement Font Assets Config, Icon Assets Config, Tailwind, Header, Navbar, Hero, About, and Skill Section.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+/**
+ * @copyright 2025 kianpamungkas
+ * @license Apache-2.0
+ */
+
+/**
+ * Components
+*/
+import About from "./components/About"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Skill from "./components/Skill"
+
+const App = () => {
+  return (
+      <>
+         <Header />
+         <main>
+            <Hero />
+            <About />
+            <Skill />
+         </main>
+      </>
+  )
+}
+
+export default App
