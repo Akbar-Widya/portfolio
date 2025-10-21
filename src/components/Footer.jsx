f5f57bed81aa65f9e654e3fe2bd90398c3f6b9bd
Akbar-Widya <akbarwidya@student.telkomuniversity.ac.id>
2025-10-22 00:34:38 +0700

    Implement Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,20 +26,16 @@ const sitemap = [
 const socials = [
   {
     label: 'GitHub',
-    href: 'https://www.github.com/codewithsadee-org'
+    href: 'https://www.github.com/Akbar-Widya'
   },
   {
     label: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/codewithsadee'
+    href: 'https://www.linkedin.com/in/kian-pamungkas'
   },
   {
     label: 'Instagram',
-    href: 'https://www.instagram.com/codewithsadee'
+    href: 'https://www.instagram.com/kianpamungkas.pmk'
   },
-  {
-    label: 'CodePen',
-    href: 'https://codepen.io/codewithsadee'
-  }
 ];
 
 const Footer = () => {
